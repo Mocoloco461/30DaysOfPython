43360352cc7b4d15cfc3f6e1984b256be1c28011
Mocoloco461 <nuhnuh0101@gmail.com>
2024-05-12 16:47:34 +0300

    Create Day_3.py

---
+++ Day_03/Day_3.py
@@ -0,0 +1,92 @@
+my_age = 20
+my_height = 1.80
+my_math = 3 +3j
+
+
+# find area of triangle
+"""
+triangle_base = int(input("enter base: "))
+triangle_height = int(input("enter height: "))
+
+print(triangle_base * triangle_height * 0.5)
+"""
+
+# Find the perimeter of traingle:
+"""
+side_a = int(input("Enter side a: "))
+side_b = int(input("Enter side b: "))
+side_c = int(input("Enter side c: "))
+perimeter_traingle = side_a + side_b + side_c
+
+
+print("The perimeter of triangle is: ",perimeter_traingle )
+"""
+
+# Find area and perimeter of rectangle
+'''
+rectangle_width = int(input("Enter the size of the wigth: "))
+rectangle_length = int(input("Enter the size of the length: "))
+
+rectangle_area = rectangle_width * rectangle_length
+rectangle_perimeter = (rectangle_width + rectangle_length) * 2
+
+print("The area of the rectangle is = ", rectangle_area)
+print("The perimeter of the rectangle is = ", rectangle_perimeter)
+'''
+
+# Find area of circle
+"""
+pi = 3.14
+
+circle_radius = float(input("Enter the radius: "))
+circle_area = circle_radius * circle_radius * pi
+circle_circumference = 2 * pi * circle_radius
+
+print("The area of circle is: ", circle_area)
+print("The  of circle is: ", circle_area)
+"""
+
+
+# I didnt got and solve the home work from q 8 to 11
+'''
+print(len("python") != len("Dragon"))
+print("on" in ("python" and "dragon"))
+print("jargon" in "I hope this course is not full of jargon") # I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+print(not "on" in ("dragon" and "python" )) # There is no 'on' in both dragon and python
+print(str(float(len("pyhon"))))
+
+is_even = ( 2 % 3 == 0)
+print("The number is even: ", is_even)
+'''
+
+# Some more fun stuff to caculate:
+'''
+float_num = 7 / 3
+print("The volume of 2.3 is equal to the int of 7/3: ", int(2.7) == float_num)
+
+print("Check if type of '10' is equal to type of 10: ", type('10') == type(10))
+
+print("Check if int('9.8') is equal to 10:", int(9.8) == 10)
+'''
+
+# Caculate pay from hours.
+'''
+work_time = int(input("Enter time of work: "))
+pay_per_hour = int(input("How much u get per hour? "))
+print("Your courect salery need to be -" , work_time * pay_per_hour, "₪")
+'''
+
+# How much secend remind u for liven base on that u will live until 100, secend result.
+'''
+user_age = int(input("What ur age? "))
+user_age_remaining = 100 - user_age
+print("You have lived for", user_age_remaining * 86400, "secend")
+'''
+
+# Just a random table to print, not sure way :)
+
+print("1 1 1 1 1")
+print("2 1 2 4 8")
+print("3 1 3 9 27")
+print("4 1 4 16 64")
+print("5 1 5 25 125")
